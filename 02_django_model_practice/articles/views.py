468984b660e72dbff9d7ee51ef300abfe1b77c87
gangbro-dev <gangbro.dev@gmail.com>
2022-09-01 17:58:05 +0900

    20220901

--- 02_django_model_practice/articles/views.py
+++ 02_django_model_practice/articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
 # Create your views here.
@@ -16,8 +16,8 @@ def new(request):
 
 def create(request):
     # 사용자의 데이터를 받아서 DB에 저장
-    title = request.GET.get('title')
-    content = request.GET.get('content')
+    title = request.POST.get('title')
+    content = request.POST.get('content')
     # DB에 저장
     # 1번 방법
     # article = Article()
@@ -31,4 +31,40 @@ def create(request):
 
     # 3번 방법
     # Article.objects.create(title=title, content=content)
-    return render(request, 'articles/create.html')
+    return redirect('articles:detail', article.pk)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article,
+    }
+    return render(request, 'articles/edit.html', context)
+
+def update(request, pk):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    article = Article.objects.get(pk=pk)
+    article.title = title
+    article.content = content
+    article.save()
+    return redirect('articles:detail', article.pk)
+
+
+def delete(request, aa):
+
+    # pk 이용해서 아티클 찾기
+    article = Article.objects.get(pk=pk)
+
+    article.delete()
+
+    return redirect('articles:index')
